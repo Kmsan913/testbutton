07439dccc0dfd50c913a621aa54da8a4c7e4b33d
Kiska Sanchez <kiskasanchez@Kiskas-MacBook-Air.local>
2022-05-19 17:54:11 -0600

    Updates

---
+++ precache-manifest.f6001020c1a7afd9258a5412f16a44b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b312d9df63e5083d8380",
+    "url": "/testbutton/static/css/main.9fcc71eb.chunk.css"
+  },
+  {
+    "revision": "b312d9df63e5083d8380",
+    "url": "/testbutton/static/js/main.b312d9df.chunk.js"
+  },
+  {
+    "revision": "a322975c65224bc82c97",
+    "url": "/testbutton/static/js/1.a322975c.chunk.js"
+  },
+  {
+    "revision": "559da49560498929e7ce",
+    "url": "/testbutton/static/js/2.559da495.chunk.js"
+  },
+  {
+    "revision": "a937fb08b9ea3c6c5bc2",
+    "url": "/testbutton/static/js/runtime~main.a937fb08.js"
+  },
+  {
+    "revision": "58c6787e7e70cc8e885189f860986d4c",
+    "url": "/testbutton/index.html"
+  }
+];
